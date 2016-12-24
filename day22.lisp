65a8e47fce7f57adc16888a5969d0b8996902060
Lucian Mogosanu <lucian.mogosanu@gmail.com>
2016-12-24 14:43:27 +0200

    day22, part2: Non-working solution
    The solution works for the small test input, but it runs out of memory
    for the problem input. Some ideas for going forward are given in the
    file.

--- day22.lisp
+++ day22.lisp
@@ -62,7 +62,6 @@ data (avail and use%)."
             (and (= (car x1) (car x2))
                  (= (cadr x1) (cadr x2))))))
 
-;; XXX: useless.
 (defun get-adjacent-nodes (node nodes)
   "Get adjacent nodes for a given node."
   (remove-if #'null
@@ -73,7 +72,6 @@ data (avail and use%)."
                      (list node node node node)
                      (list '(0 . 1) '(1 . 0) '(0 . -1) '(-1 . 0)))))
 
-;; XXX: useless.
 (defun adjacent-node-pairs (nodes)
   "Get all pairs of adjacent nodes."
   (apply #'concatenate 'list
@@ -105,10 +103,201 @@ data (avail and use%)."
   "Return all the viable pairs in a list of nodes."
   (remove-if-not #'is-viable? (node-pairs nodes)))
 
+(defun viable-adjacent-pairs (nodes)
+  "Return all the viable pairs in a list of nodes."
+  (remove-if-not #'is-viable? (adjacent-node-pairs nodes)))
+
+;; Part 2
+;
+; Again, an optimization problem. We want to move the data from
+; node-xn-y0 (where n is the highest index there is) to node-x0-y0. For
+; this, we need to move data around. We're given a test example for
+; which n = 2.
+;;
+
+; State representation and manipulation
+(defstruct (state (:constructor mk-state0 (&key data-pos nodes)))
+  data-pos nodes)
+
+(defun max-x (nodes)
+  "Get the maximum x from a list of nodes."
+  (reduce #'(lambda (acc node)
+              (max acc (car node)))
+          nodes :initial-value 0))
+
+(defun make-state (nodes)
+  "Make a fresh initial state from nodes."
+  (mk-state0 :data-pos (cons (max-x nodes) 0)
+             :nodes nodes))
+
+(defmacro find-node-pos (pos nodes)
+  "Find a node based on a pos."
+  `(find-node (list (car ,pos) (cdr ,pos) 0 0) ,nodes))
+
+(defun move-data (state old-data-pos new-data-pos)
+  "Move data in state to new-data-pos."
+  (let* ((data-pos (state-data-pos state))
+         (nodes (state-nodes state))
+         (src-node (find-node-pos old-data-pos nodes))
+         (dest-node (find-node-pos new-data-pos nodes))
+         (new-src-node (list (car src-node) (cadr src-node)
+                             (caddr src-node) 0))
+         (new-dest-node (list (car dest-node) (cadr dest-node)
+                                        (caddr dest-node)
+                                        (+ (cadddr dest-node)
+                                           (cadddr src-node)))))
+    (mk-state0 :data-pos (if (equal data-pos old-data-pos)
+                             new-data-pos
+                             data-pos)
+               :nodes (substitute new-dest-node
+                                  dest-node
+                                  (substitute new-src-node
+                                              src-node
+                                              nodes
+                                              :test 'equal)
+                                  :test 'equal))))
+
+;; High-level state space exploration functionality
+(defun states-from (state)
+  "Return all the reachable states from state."
+  (mapcar #'(lambda (pair)
+              (let* ((src-node (car pair))
+                     (dest-node (cdr pair)))
+                (move-data state
+                           (cons (car src-node) (cadr src-node))
+                           (cons (car dest-node) (cadr dest-node)))))
+          (viable-adjacent-pairs (state-nodes state))))
+
+(defun is-final-state? (state)
+  "Is state final?"
+  (equal (state-data-pos state) '(0 . 0)))
+
+(defun adjacent-nodes-used (node nodes)
+  "Get the used disk data in each adjacent node to node."
+  (mapcar #'cadddr (get-adjacent-nodes node nodes)))
+
+(defun score (state)
+  "Evaluate the score of a given state."
+  (let* ((data-pos (state-data-pos state)))
+    (+ (car data-pos) (cdr data-pos))))
+
+(defun sort-states (state-list)
+  "Destructively sort a list of states."
+  (sort state-list #'(lambda (s1 s2) (< (score s1) (score s2)))))
+
+(defun is-discovered? (state discovered)
+  "Was a state previously discovered?"
+  (assoc state discovered :test 'equal))
+
+;; State space exploration algorithm(s)
+(defun explore-bfs (discovered queue)
+  "(Destructively) explore using BFS starting from queue state."
+  (do ((is-final nil))
+      ((or is-final (null queue)) (cons is-final discovered))
+    (let* ((p (pop queue))
+           (state (car p))
+           (new-states (sort-states (states-from state))))
+      (push p discovered)
+      (loop for new-state in new-states do
+           (when (not (is-discovered? new-state discovered))
+             (setq queue (nconc queue (list (cons new-state state)))))
+           (when (is-final-state? new-state)
+             (push (cons new-state state) discovered)
+             (setq is-final t)
+             (return)
+             )))))
+
+(defun explore-dfs (discovered stack)
+  "(Destructively) explore using DFS starting from stack state."
+  (do ((is-final nil))
+      ((or is-final (null stack)) (cons is-final discovered))
+    (let* ((p (pop stack))
+           (state (car p))
+           (new-states (sort-states (states-from state))))
+      (push p discovered)
+      (loop for new-state in new-states do
+           (when (not (is-discovered? new-state discovered))
+             (push (cons new-state state) stack))
+           (when (is-final-state? new-state)
+             (push (cons new-state state) discovered)
+             (setq is-final t)
+             (return)
+             )))))
+
+;; Further thought on problem:
+;
+; Naive DFS/BFS approaches don't work here, even with a search
+; heuristic, as the state space seems to be huge. The program runs out
+; of memory after a few minutes of search.
+;
+; Idea: the problem can be broken into small iterative „find the
+; shortest path from node-x{i}-y0 to node-x{i-1}-y0” steps, until
+; node-x0-y0 is reached. The fact that we start on column 0 seems to
+; indicate that we should be able to move the data only on column 0 --
+; for the given input, in general this might not hold.
+;
+; TODO this: ^
+;;
+
+(defun find-final-state (discovered)
+  "Find the final state in discovered."
+  (let ((edge (find-if #'(lambda (pair)
+                       (equal (state-data-pos (car pair))
+                              '(0 . 0)))
+                       discovered)))
+    (when edge
+      (car edge))))
+
+(defun find-path (initial final discovered)
+  "Find a path from initial to final in discovered."
+  (if (equal initial final)
+      (list initial)
+      (cons final
+            (let ((new-final (cdr (is-discovered? final discovered))))
+              (find-path initial new-final discovered)))))
+
 ;; Tests
+
+;; Part 1
 (let ((nodes (with-open-file (in "day22-input")
                (parse-input in))))
   (format t "## Test 1: from day22-input~%")
   (let ((viable (viable-pairs nodes)))
    (format t "Viable pairs are: ~s~%(total ~d)~%"
            viable (length viable))))
+
+;; Part 2
+(defvar test-input
+  "$ df -h
+Filesystem            Size  Used  Avail  Use%
+/dev/grid/node-x0-y0   10T    8T     2T   80%
+/dev/grid/node-x0-y1   11T    6T     5T   54%
+/dev/grid/node-x0-y2   32T   28T     4T   87%
+/dev/grid/node-x1-y0    9T    7T     2T   77%
+/dev/grid/node-x1-y1    8T    0T     8T    0%
+/dev/grid/node-x1-y2   11T    7T     4T   63%
+/dev/grid/node-x2-y0   10T    6T     4T   60%
+/dev/grid/node-x2-y1    9T    8T     1T   88%
+/dev/grid/node-x2-y2    9T    6T     3T   66%
+")
+; Test
+(let* ((test-nodes (with-input-from-string (in test-input)
+                    (parse-input in)))
+       (initial-state (make-state test-nodes)))
+  (format t "## Test 0.2: input from test-input...~%")
+  (let* ((explore (explore-bfs nil (list (cons initial-state 'none))))
+         (discovered (cdr explore))
+         (final-state (find-final-state discovered))
+         (path (find-path initial-state final-state discovered)))
+    (format t "Found solution of length ~d.~%" (length path))))
+
+; Actual run
+(let* ((my-nodes (with-open-file (in "day22-input")
+                    (parse-input in)))
+       (initial-state (make-state my-nodes)))
+  (format t "## Test 2: input from day22-input...~%")
+  (let* ((explore (explore-dfs nil (list (cons initial-state 'none))))
+         (discovered (cdr explore))
+         (final-state (find-final-state discovered))
+         (path (find-path initial-state final-state discovered)))
+    (format t "Found solution of length ~d.~%" (length path))))
